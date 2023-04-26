7a8932b058089901e1b66f3b792fd838e8bf05eb
oscarparedez <oaparedez17@gmail.com>
2023-04-25 18:25:42 -0600

    update try digits

--- result.py
+++ result.py
@@ -3,11 +3,15 @@ print("Hola")
 #Lexer para Gramática No. 1 - Expresiones aritméticas simples para variables
 #
 print('WS')
-if True: print("IDlllll")
+if True: print("ID1")
 print('WS')
 print('TIMES')
 print('WS')
 print('LPAREN')
 print('RPAREN')
+print('WS')
+if True: print("ID1")
+print('WS')
+print('ID2')
 print("adios")
 #
